8ffe107e641d96becddbde8c03b282aa2a213443
Rliop913 <87229271+Rliop913@users.noreply.github.com>
2026-04-30 00:03:39 +0900

    patched cicd build errors

--- cmake/godotExternalSetter.cmake
+++ cmake/godotExternalSetter.cmake
@@ -2,81 +2,24 @@
 
 include(cmake/godotArchNameSetter.cmake)
 
+if(BUILD_TYPE_LOWER STREQUAL "debug")
+    set(GODOTCPP_TARGET "template_debug")
+else()
+    set(GODOTCPP_TARGET "template_release")
+endif()
+
 if(WIN32)
 
-    set(PLATFORM_BUILD_PATH  "${PLATFORM_ID_LOWER}-debug")
-    ExternalProject_Add(godot_cpp_external_debug
-    PREFIX          ${CMAKE_BINARY_DIR}/_deps/godotcpp/${PLATFORM_BUILD_PATH}
-    GIT_REPOSITORY  https://github.com/godotengine/godot-cpp.git
-    GIT_TAG         godot-4.5-stable
-    BINARY_DIR      ${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/_install/godotcpp/${PLATFORM_BUILD_PATH}
-        -DTYPED_METHOD_BIND:BOOL=ON
-        -DGODOTCPP_DEBUG_CRT:BOOL=ON
-        -DGODOTCPP_USE_STATIC_CPP:BOOL=OFF
-        -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebugDLL
-
-        BUILD_COMMAND ${CMAKE_COMMAND} --build . --config Debug
-        INSTALL_COMMAND ""
-    )
-
-    set(PLATFORM_BUILD_PATH  "${PLATFORM_ID_LOWER}-release")
-    ExternalProject_Add(godot_cpp_external_release
-    PREFIX          ${CMAKE_BINARY_DIR}/_deps/godotcpp/${PLATFORM_BUILD_PATH}
-    GIT_REPOSITORY  https://github.com/godotengine/godot-cpp.git
-    GIT_TAG         godot-4.5-stable
-    BINARY_DIR      ${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/_install/godotcpp/${PLATFORM_BUILD_PATH}
-        -DTYPED_METHOD_BIND:BOOL=ON
-        -DGODOTCPP_DEBUG_CRT=OFF
-        -DGODOTCPP_USE_STATIC_CPP=OFF
-        -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL
-        -DGODOTCPP_TARGET=template_release
-
-        BUILD_COMMAND ${CMAKE_COMMAND} --build . --config Release
-        INSTALL_COMMAND ""
-    )
-    
-    ExternalProject_Get_Property(godot_cpp_external_release source_dir binary_dir install_dir)
-elseif(APPLE)
-
-    set(PLATFORM_BUILD_PATH  "${PLATFORM_ID_LOWER}-${CMAKE_BUILD_TYPE}")
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set(GODOTCPP_USE_HOT_RELOAD OFF CACHE BOOL "Disable hot reload" FORCE)
-        ExternalProject_Add(godot_cpp_external
-        PREFIX          ${CMAKE_BINARY_DIR}/_deps/godotcpp/${PLATFORM_BUILD_PATH}
-        GIT_REPOSITORY  https://github.com/godotengine/godot-cpp.git
-        GIT_TAG         godot-4.5-stable
-        BINARY_DIR      ${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}
-        CMAKE_ARGS
-            -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/_install/godotcpp/${PLATFORM_BUILD_PATH}
-            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-            -DGODOTCPP_TARGET=template_${BUILD_TYPE_LOWER}
-            -DGODOTCPP_USE_HOT_RELOAD=OFF
-
-            INSTALL_COMMAND ""
-            BUILD_BYPRODUCTS "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}/bin/libgodot-cpp.macos.template_${BUILD_TYPE_LOWER}.${ARCH_LOWER}.a"
-        )
+    set(PLATFORM_BUILD_PATH  "${PLATFORM_ID_LOWER}-${BUILD_TYPE_LOWER}")
+    if(GODOTCPP_TARGET STREQUAL "template_debug")
+        set(GODOTCPP_DEBUG_CRT ON)
+        set(GODOTCPP_MSVC_RUNTIME_LIBRARY "MultiThreadedDebugDLL")
     else()
-        ExternalProject_Add(godot_cpp_external
-        PREFIX          ${CMAKE_BINARY_DIR}/_deps/godotcpp/${PLATFORM_BUILD_PATH}
-        GIT_REPOSITORY  https://github.com/godotengine/godot-cpp.git
-        GIT_TAG         godot-4.5-stable
-        BINARY_DIR      ${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}
-        CMAKE_ARGS
-            -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/_install/godotcpp/${PLATFORM_BUILD_PATH}
-            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-            -DGODOTCPP_TARGET=template_${BUILD_TYPE_LOWER}
-
-            INSTALL_COMMAND ""
-            BUILD_BYPRODUCTS "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}/bin/libgodot-cpp.macos.template_${BUILD_TYPE_LOWER}.${ARCH_LOWER}.a"
-        )
+        set(GODOTCPP_DEBUG_CRT OFF)
+        set(GODOTCPP_MSVC_RUNTIME_LIBRARY "MultiThreadedDLL")
     endif()
-    ExternalProject_Get_Property(godot_cpp_external source_dir binary_dir install_dir)
-else()
-    set(PLATFORM_BUILD_PATH  "${PLATFORM_ID_LOWER}-${CMAKE_BUILD_TYPE}")
+    set(GODOTCPP_LIBRARY "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}/bin/libgodot-cpp.${PLATFORM_ID_LOWER}.${GODOTCPP_TARGET}.${ARCH_LOWER}.lib")
+
     ExternalProject_Add(godot_cpp_external
     PREFIX          ${CMAKE_BINARY_DIR}/_deps/godotcpp/${PLATFORM_BUILD_PATH}
     GIT_REPOSITORY  https://github.com/godotengine/godot-cpp.git
@@ -85,12 +28,54 @@ else()
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/_install/godotcpp/${PLATFORM_BUILD_PATH}
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DGODOTCPP_TARGET=template_${BUILD_TYPE_LOWER}
+        -DTYPED_METHOD_BIND:BOOL=ON
+        -DGODOTCPP_DEBUG_CRT:BOOL=${GODOTCPP_DEBUG_CRT}
+        -DGODOTCPP_USE_STATIC_CPP:BOOL=OFF
+        -DCMAKE_MSVC_RUNTIME_LIBRARY=${GODOTCPP_MSVC_RUNTIME_LIBRARY}
+        -DGODOTCPP_TARGET=${GODOTCPP_TARGET}
+
+    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE}
+    INSTALL_COMMAND ""
+    BUILD_BYPRODUCTS "${GODOTCPP_LIBRARY}"
+    )
+
+    ExternalProject_Get_Property(godot_cpp_external source_dir binary_dir install_dir)
+elseif(APPLE)
+
+    set(PLATFORM_BUILD_PATH  "${PLATFORM_ID_LOWER}-${BUILD_TYPE_LOWER}")
+    set(GODOTCPP_LIBRARY "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}/bin/libgodot-cpp.macos.${GODOTCPP_TARGET}.${ARCH_LOWER}.a")
+    ExternalProject_Add(godot_cpp_external
+    PREFIX          ${CMAKE_BINARY_DIR}/_deps/godotcpp/${PLATFORM_BUILD_PATH}
+    GIT_REPOSITORY  https://github.com/godotengine/godot-cpp.git
+    GIT_TAG         godot-4.5-stable
+    BINARY_DIR      ${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/_install/godotcpp/${PLATFORM_BUILD_PATH}
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DGODOTCPP_TARGET=${GODOTCPP_TARGET}
+        -DGODOTCPP_USE_HOT_RELOAD:BOOL=OFF
+
+    INSTALL_COMMAND ""
+    BUILD_BYPRODUCTS "${GODOTCPP_LIBRARY}"
+    )
+    ExternalProject_Get_Property(godot_cpp_external source_dir binary_dir install_dir)
+else()
+    set(PLATFORM_BUILD_PATH  "${PLATFORM_ID_LOWER}-${BUILD_TYPE_LOWER}")
+    set(GODOTCPP_LIBRARY "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}/bin/libgodot-cpp.${PLATFORM_ID_LOWER}.${GODOTCPP_TARGET}.${ARCH_LOWER}.a")
+    ExternalProject_Add(godot_cpp_external
+    PREFIX          ${CMAKE_BINARY_DIR}/_deps/godotcpp/${PLATFORM_BUILD_PATH}
+    GIT_REPOSITORY  https://github.com/godotengine/godot-cpp.git
+    GIT_TAG         godot-4.5-stable
+    BINARY_DIR      ${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/_install/godotcpp/${PLATFORM_BUILD_PATH}
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DGODOTCPP_TARGET=${GODOTCPP_TARGET}
         -DCMAKE_C_COMPILER=clang
         -DCMAKE_CXX_COMPILER=clang++
         
         INSTALL_COMMAND ""
-        BUILD_BYPRODUCTS "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}/bin/libgodot-cpp.${PLATFORM_ID_LOWER}.template_${BUILD_TYPE_LOWER}.${ARCH_LOWER}.a"
+        BUILD_BYPRODUCTS "${GODOTCPP_LIBRARY}"
     )
     ExternalProject_Get_Property(godot_cpp_external source_dir binary_dir install_dir)
 endif()
@@ -98,19 +83,18 @@ add_library(godotcppEx STATIC IMPORTED GLOBAL)
 if(WIN32)
 
     set_target_properties(godotcppEx PROPERTIES
-    IMPORTED_CONFIGURATIONS "Debug;Release"
-    IMPORTED_LOCATION_DEBUG "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_ID_LOWER}-debug/bin/libgodot-cpp.${PLATFORM_ID_LOWER}.template_debug.${ARCH_LOWER}.lib"
-    IMPORTED_LOCATION_RELEASE "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_ID_LOWER}-release/bin/libgodot-cpp.${PLATFORM_ID_LOWER}.template_release.${ARCH_LOWER}.lib"
-        MSVC_RUNTIME_LIBRARY "$<$<CONFIG:Debug>:MultiThreadedDebugDLL>$<$<CONFIG:Release>:MultiThreadedDLL>"
+    IMPORTED_LOCATION "${GODOTCPP_LIBRARY}"
+        MSVC_RUNTIME_LIBRARY "${GODOTCPP_MSVC_RUNTIME_LIBRARY}"
     )
-    add_dependencies(godotcppEx godot_cpp_external_debug godot_cpp_external_release)
+    add_dependencies(godotcppEx godot_cpp_external)
 elseif(APPLE)
 set_target_properties(godotcppEx PROPERTIES
-    IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}/bin/libgodot-cpp.macos.template_${BUILD_TYPE_LOWER}.${ARCH_LOWER}.a"
+    IMPORTED_LOCATION "${GODOTCPP_LIBRARY}"
     )
+    add_dependencies(godotcppEx godot_cpp_external)
 else()
     set_target_properties(godotcppEx PROPERTIES
-    IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/_build/godotcpp/${PLATFORM_BUILD_PATH}/bin/libgodot-cpp.${PLATFORM_ID_LOWER}.template_${BUILD_TYPE_LOWER}.${ARCH_LOWER}.a"
+    IMPORTED_LOCATION "${GODOTCPP_LIBRARY}"
     )
     add_dependencies(godotcppEx godot_cpp_external)
 endif()
